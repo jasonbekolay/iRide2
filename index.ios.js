0b5bcb39eeea12b2f4fe9b3cbde42c9e3c476cf7
Jason Bekolay <beek@Jasons-MacBook-Pro.local>
2015-05-10 22:36:11 -0500

    Fetch stop and display name

--- index.ios.js
+++ index.ios.js
@@ -10,14 +10,46 @@ var {
   StyleSheet,
   Text,
   View,
+  ListView
 } = React;
 
+var STOP_URL = 'http://api.winnipegtransit.com/v2/stops/60239/schedule.json?api-key=7z8e2GUnJZ6Z17x2iueI';
+
 var iRide2 = React.createClass({
+  getInitialState: function() {
+    return { stopSchedule: null };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(STOP_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          stopSchedule: responseData['stop-schedule']
+        })
+      })
+      .done();
+  },
   render: function() {
+    if (!this.state.stopSchedule) {
+      return this.renderLoading();
+    }
+    return this.renderStop(this.state.stopSchedule);
+  },
+  renderLoading: function() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  },
+  renderStop: function(stop) {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {stop.stop.name}
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
